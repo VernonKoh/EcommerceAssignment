db076eb15f634efb791b2d972a4c671c048676b9
astronny <aar0planee@gmail.com>
2024-01-23 00:39:24 +0800

    some shoppingcart, refresh

---
+++ checkLogin.php
@@ -0,0 +1,86 @@
+<?php
+// Detect the current session
+session_start();
+// Include the Page Layout header
+include("header.php");
+
+// Reading inputs entered in the previous page
+$email = isset($_POST["email"]) ? trim($_POST["email"]) : "";
+$password = isset($_POST["password"]) ? trim($_POST["password"]) : "";
+
+// Validate email and password length
+if (strlen($email) == 0 || strlen($password) == 0) {
+    echo "<h3 style='color:red'>Invalid Login Credentials</h3>";
+    echo '<a href="login.php">Please try logging in again</a>';
+    include("footer.php");
+    exit;
+}
+
+// To Do 1 (Practical 2): Validate login credentials with the database
+include_once("mysql_conn.php");
+
+// Validate login credentials with the database
+$qry = "SELECT * FROM Shopper WHERE Email=?";
+$stmt = $conn->prepare($qry);
+$stmt->bind_param("s", $email);
+
+if ($stmt->execute()) {
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+
+    if ($row && password_verify($password, $row["Password"])) {
+        // Valid login
+        $_SESSION["ShopperName"] = $row["Name"];
+        $_SESSION["ShopperID"] = $row["ShopperID"];
+
+        // To Do 2 (Practical 4): Get active shopping cart
+        $qry = "SELECT ShopCartID FROM ShopCart WHERE ShopperID=? AND OrderPlaced=0";
+        $stmt = $conn->prepare($qry);
+        $stmt->bind_param("i", $_SESSION["ShopperID"]);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+
+        if ($result->num_rows > 0) {
+            // Active shopping cart found, retrieve ShopCartID
+            $row = $result->fetch_assoc();
+            // Get the hashed password from database
+            $hashed_password = $row["Password"];
+            // Verifies that a password matches a hash
+            if (password_verify($password, $hashed_password) == true) {
+                $_SESSION["Cart"] = $row["ShopCartID"];
+
+                // Count the number of uncheckout items
+                $qry = "SELECT COUNT(*) AS NumItems FROM ShopCartItem WHERE ShopCartID=?";
+                $stmt = $conn->prepare($qry);
+                $stmt->bind_param("i", $_SESSION["Cart"]);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                $stmt->close();
+
+                $row = $result->fetch_assoc();
+                $_SESSION["NumCartItem"] = $row["NumItems"];
+            }
+        } else {
+            // No active shopping cart found
+            $_SESSION["Cart"] = null;
+            $_SESSION["NumCartItem"] = 0;
+        }
+
+        $conn->close();
+        // Redirect to the home page
+        header("Location: index.php");
+        exit;
+    } else {
+        // Invalid login credentials
+        echo "<h3 style='color:red'>Invalid Login Credentials</h3>";
+        echo '<a href="login.php">Please try logging in again</a>';
+    }
+} else {
+    // Handle database query execution error
+    echo "<h3 style='color:red'>Error executing database query</h3>";
+}
+
+// Include the Page Layout footer
+include("footer.php");
+?>
--- search.php
+++ search.php
@@ -2,12 +2,12 @@
 // The non-empty search keyword is sent to server
 if (isset($_GET["keywords"]) && trim($_GET['keywords']) != "") {
     // To Do (DIY): Retrieve list of product records with "ProductTitle" 
-	// contains the keyword entered by shopper, and display them in a table.
-	
+    // contains the keyword entered by shopper, and display them in a table.
+
     // Include the PHP file that establishes database connection handle: $conn
     include_once("mysql_conn.php");
     // To Do (DIY): Starting ....
-    $SearchText="%".$_GET["keywords"]."%"; // Read Category ID from query string
+    $SearchText = "%" . $_GET["keywords"] . "%"; // Read Category ID from query string
     echo "<p> Search Results for <b> $_GET[keywords] </b>: </p>";
 
     // Form SQL to retrieve list of products associated to the Search Text
@@ -19,16 +19,16 @@ if (isset($_GET["keywords"]) && trim($_GET['keywords']) != "") {
 
     $stmt = $conn->prepare($qry);
     // $SearchText = "%".$SearchText."%";
-    $stmt->bind_param("sss", $SearchText, $SearchText,$SearchText); // "i" integer
+    $stmt->bind_param("sss", $SearchText, $SearchText, $SearchText); // "i" integer
     $stmt->execute();
-    $result = $stmt->get_result(); 
+    $result = $stmt->get_result();
     $stmt->close();
     // Display each product in a row
-    if($result->num_rows>0) { // If found, display records
-        
-        
-        while($row= $result-> fetch_array()){
-            
+    if ($result->num_rows > 0) { // If found, display records
+
+
+        while ($row = $result->fetch_array()) {
+
             echo "<div class='row' style='padding:5px'>"; // Start a new row
             // Left column - display a text link showing the product's name,
             // display the selling price in red in a new paragraph
@@ -41,7 +41,7 @@ if (isset($_GET["keywords"]) && trim($_GET['keywords']) != "") {
             echo "</div>";
 
             // Right column display the product's image
-            $img="./Images/products/$row[ProductImage]";
+            $img = "./Images/products/$row[ProductImage]";
             echo "<div class='col-4'>"; //33% of row width
             echo "<img src='$img' />";
             echo "</div>";
@@ -49,13 +49,12 @@ if (isset($_GET["keywords"]) && trim($_GET['keywords']) != "") {
             echo "</div>"; // End of a row
 
         }
+    } else {
+        echo " No records found!";
     }
-else{
-    echo " No records found!";
-}
 
 
-	// To Do (DIY): End of Code
+    // To Do (DIY): End of Code
 }
 
 echo "</div>"; // End of container
